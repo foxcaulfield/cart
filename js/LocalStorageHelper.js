8b18880f2557b800149e4a65319ad247d1f1f8cb
foxcaulfield <fox.caulfield@gmail.com>
2024-02-25 20:46:54 +0100

    Add js file for local storage handling

---
+++ js/LocalStorageHelper.js
@@ -0,0 +1,20 @@
+export class LocalStorageHelper {
+    constructor() {
+        // console.log("LS created!");
+    }
+
+    setItem(code, value) {
+        localStorage.setItem(code, JSON.stringify(value));
+    }
+
+    hasItem(code) {
+        return localStorage.getItem(code) !== null;
+    }
+
+    pushToItem(code, value) {
+        const item = JSON.parse(localStorage.getItem(code) || []);
+        item.push(value);
+        localStorage.setItem(code, JSON.stringify(item));
+    }
+}
+
